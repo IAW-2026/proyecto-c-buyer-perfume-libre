ba43da36369aa487559791a9f973a1a35c8e934e
Nicolas Bernardini <nicolasbernardini54@gmail.com>
2026-05-08 17:57:03 -0300

    feat: implementar componente ProductGrid

---
+++ components/home/productGrid.tsx
@@ -0,0 +1,14 @@
+import ProductCard from "@/components/home/productCard";
+import { getPerfumes } from "@/lib/data";
+
+export default async function ProductGrid() {
+  const perfumes = await getPerfumes();
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+      {perfumes.map((perfume) => (
+        <ProductCard key={perfume.id} {...perfume} />
+      ))}
+    </div>
+  );
+}
