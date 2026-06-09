bbdcb6639a0e3563be146608807e4810c9d5091f
Nicolas Bernardini <nicolasbernardini54@gmail.com>
2026-06-09 14:58:47 -0300

    style: mejorar diseño del componente ProductCard y agregar calificación de estrellas

--- components/home/productGrid.tsx
+++ components/home/productGrid.tsx
@@ -23,7 +23,7 @@ export default async function ProductGrid({
 
   return (
     <div className="space-y-10">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-6">
         {items.map((perfume) => (
           <ProductCard key={perfume.id} {...perfume} />
         ))}
