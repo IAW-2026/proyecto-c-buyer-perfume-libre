31378fec850c5f2c38af29eb1d08836841df8441
Nicolas Bernardini <nicolasbernardini54@gmail.com>
2026-06-11 17:29:53 -0300

    feat: agregar navegador

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -4,6 +4,7 @@ import { prisma } from "@/lib/prisma";
 import { RolUsuario } from "@/lib/generated/prisma/client";
 import { obtenerRolUsuario } from "@/actions/usuario";
 import AdminHeader from "@/components/admin/AdminHeader";
+import AdminNav from "@/components/admin/AdminNav";
 
 export default async function AdminLayout({
   children,
@@ -21,6 +22,8 @@ export default async function AdminLayout({
       <AdminHeader />
 
       <main className="flex-1 container mx-auto p-4 md:p-8 max-w-7xl animate-in fade-in duration-300">
+        <AdminNav />
+
         {children}
       </main>
     </div>
